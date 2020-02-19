8d1cd2c17c451c79c10dcb9e4a40ce43cb712ec5
Hung Nguyen Xuan <hungnx@pixelz.com>
2020-02-20 00:14:35 +0700

    update time & conversations

--- index-script.js
+++ index-script.js
@@ -8,8 +8,8 @@ window.onload = () => {
 
 var currentUsername = 'nguyenxuan.hung96@gmail.com';
 
-function sendMessage($event){
-  if($event.keyCode === 13){
+function sendMessage($event) {
+  if ($event.keyCode === 13) {
     let m = {
       createBy: currentUsername,
       createAt: new Date().toISOString(),
@@ -20,15 +20,15 @@ function sendMessage($event){
   }
 }
 
-function addMessage(message){
-  if(message.createBy === currentUsername){
+function addMessage(message) {
+  if (message.createBy === currentUsername) {
     addOutcomeMessage(message);
-  }else{
+  } else {
     addIncomeMessage(message);
   }
 }
 
-function addOutcomeMessage(message){
+function addOutcomeMessage(message) {
   $('#chat-window').append(`
     <div class="chatbox__messages outcome-message animated fadeIn">
       <div class="chatbox__messages__user-message">
@@ -41,7 +41,7 @@ function addOutcomeMessage(message){
   `);
   $('#chat-window').scrollTop($('#chat-window')[0].scrollHeight);
 }
-function addIncomeMessage(message){
+function addIncomeMessage(message) {
   $('#chat-window').append(`
     <div class="chatbox__messages income-message">
       <div class="chatbox__messages__user-message">
@@ -56,11 +56,11 @@ function addIncomeMessage(message){
   $('#chat-window').scrollTop($('#chat-window')[0].scrollHeight);
 }
 
-function formatDatetime(d){
-  return moment(d, 'dd-MM-yyyy HH:mm:ss');
+function formatDatetime(d) {
+  return moment(d).format('DD-MM-YYYY HH:mm:ss');
 }
 
-async function sendToDB(newMessage){
+async function sendToDB(newMessage) {
   const db = firebase.firestore();
 
   await db.collection(db_collection).doc('aPkkl30lzWuqQBvDMX0I').update({
@@ -68,26 +68,39 @@ async function sendToDB(newMessage){
   });
 }
 
-async function loadConversation(){
+function addConversations(conversations) {
+  $('.chatbox_conversations').html('');
+  if (conversations) {
+    for (const c of conversations) {
+      $('.chatbox_conversations').append(`
+      <div class='chatbox__user--active'>
+        <p>${c.name}</p>
+      </div>
+      `);
+    }
+  }
+}
+
+async function loadConversation() {
   const db = firebase.firestore();
   db.collection(db_collection).where('members', 'array-contains', currentUsername)
-    .onSnapshot(function (snapShot){
-      if(!conversations){
+    .onSnapshot(function (snapShot) {
+      if (!conversations) {
 
-        conversations = snapShot.docChanges().map(function(item){
+        conversations = snapShot.docChanges().map(function (item) {
           return {
             id: item.doc.id,
             ...item.doc.data(),
           };
         });
-        if(conversations && conversations.length > 0){
+        if (conversations && conversations.length > 0) {
           activedConversation = conversations[0];
 
           for (let message of activedConversation.messages) {
             addMessage(message);
           }
         }
-      }else{
+      } else {
         for (const item of snapShot.docChanges()) {
           let c = {
             id: item.doc.id,
@@ -95,22 +108,23 @@ async function loadConversation(){
           };
 
           for (let i = 0; i < conversations.length; i++) {
-            if(conversations[i].id === c.id){
+            if (conversations[i].id === c.id) {
               conversations[i] = c;
-            }            
+            }
           };
 
-          if(c.id === activedConversation.id){
+          if (c.id === activedConversation.id) {
             activedConversation = c;
             addMessage(c.messages[c.messages.length - 1]);
           }
         }
       }
+      addConversations(conversations);
     })
 }
 
-function selectUser(target){
-  if(target.value){
+function selectUser(target) {
+  if (target.value) {
     currentUsername = target.value;
     loadConversation();
   }
