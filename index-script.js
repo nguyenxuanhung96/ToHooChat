e6620f6f9a690c3b4421fb0c09c35c4c7171b669
Hung Nguyen Xuan <hungnx@pixelz.com>
2020-02-20 08:44:43 +0700

    update

--- index-script.js
+++ index-script.js
@@ -10,7 +10,7 @@ var currentUsername;
 
 function sendMessage($event) {
   if ($event.keyCode === 13) {
-    if(!currentUsername){
+    if (!currentUsername) {
       alert('Select acc first!');
       return;
     }
@@ -67,7 +67,7 @@ function formatDatetime(d) {
 async function sendToDB(newMessage) {
   const db = firebase.firestore();
 
-  await db.collection(db_collection).doc('aPkkl30lzWuqQBvDMX0I').update({
+  await db.collection(db_collection).doc(activedConversation.id).update({
     messages: firebase.firestore.FieldValue.arrayUnion(newMessage),
   });
 }
@@ -77,7 +77,7 @@ function addConversations(conversations) {
   if (conversations) {
     for (const c of conversations) {
       $('.chatbox_conversations').append(`
-      <div class='chatbox__user--active'>
+      <div class='chatbox__user' ref="${c.id}" onclick="onSelectedConversation('${c.id}');">
         <p>${c.name}</p>
       </div>
       `);
@@ -100,6 +100,7 @@ async function loadConversation() {
         if (conversations && conversations.length > 0) {
           activedConversation = conversations[0];
 
+          $('#chat-window').html('');
           for (let message of activedConversation.messages) {
             addMessage(message);
           }
@@ -119,12 +120,38 @@ async function loadConversation() {
 
           if (c.id === activedConversation.id) {
             activedConversation = c;
-            addMessage(c.messages[c.messages.length - 1]);
+
+            if (c.messages.length > 0) {
+              addMessage(c.messages[c.messages.length - 1]);
+            }
           }
         }
       }
       addConversations(conversations);
-    })
+      activeUIActiveConversation();
+    });
+};
+
+function onSelectedConversation(id) {
+  for (const c of conversations) {
+    if (c.id === id) {
+      activedConversation = c;
+      reloadConversation();
+    }
+  }
+  activeUIActiveConversation();
+}
+
+function reloadConversation() {
+  $('#chat-window').html('');
+  for (let message of activedConversation.messages) {
+    addMessage(message);
+  }
+}
+
+function activeUIActiveConversation(){
+  $('.chatbox_conversations .chatbox__user').removeClass('active');
+  $(`.chatbox_conversations .chatbox__user[ref="${activedConversation.id}"]`).addClass('active');
 }
 
 function selectUser(target) {
