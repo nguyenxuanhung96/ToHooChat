ed75a0a03b36e9a9e0404ea803e32b16e23b3608
Hung Nguyen Xuan <hungnx@pixelz.com>
2020-02-20 22:41:04 +0700

    show error

--- login/login.js
+++ login/login.js
@@ -4,17 +4,23 @@ $('#form-info').submit(async function (e) {
 
   let email = this.elements['inputEmail'].value;
   let pass = this.elements['inputPassword'].value;
-  const loginResult = await firebase.auth().signInWithEmailAndPassword(email, pass);
-  if (!loginResult.user.emailVerified) {
-    alert("Please verify your email first");
-    return;
-  }
-  else {
-    localStorage.setItem('currentUser', JSON.stringify({
-      uid: loginResult.user.id,
-      displayName: loginResult.user.displayName,
-      email: loginResult.user.email,
-    }));
-    window.location.href = '../index.html';
+  try {
+
+    const loginResult = await firebase.auth().signInWithEmailAndPassword(email, pass);
+    if (!loginResult.user.emailVerified) {
+      alert("Please verify your email first");
+      return;
+    }
+    else {
+      localStorage.setItem('currentUser', JSON.stringify({
+        uid: loginResult.user.id,
+        displayName: loginResult.user.displayName,
+        email: loginResult.user.email,
+      }));
+      window.location.href = '../index.html';
+    }
+  }catch(error){
+    console.log(error);
+    alert(error.message);
   }
 })
